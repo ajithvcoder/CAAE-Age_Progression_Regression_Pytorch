c12891cc858742ced308bab55640cafa6a028205
ajithkumar.v <ajithkumar.v@lotte.net>
2021-01-31 23:59:29 +0530

    models fixed

--- models.py
+++ models.py
@@ -1,261 +1,146 @@
-## import models
-
 import torch
 from torch import nn
 from torch import optim
 from torch.autograd import Variable
 
-# parameters 
-# initial channel numbers 
+
 n_channel = 3
-# discriminator base output-16
 n_disc = 16
-# generator base output 64 
 n_gen = 64
-# encoder base 64
 n_encode = 64
-# label number
 n_l = 10
-# latent vector size
 n_z = 50
-# size of image 
 img_size = 128
-# batchsize with which training takes place
 batchSize = 20
 use_cuda = torch.cuda.is_available()
-# age category is 50/10 = 5
 n_age = int(n_z/n_l)
-# gender is 50/2 = 25
 n_gender = int(n_z/2)
 
-# adopted from 
-# https://github.com/dxyang/StyleTransfer/blob/master/network.py
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder,self).__init__()
+        self.conv = nn.Sequential(
+            #input: 3*128*128
+            nn.Conv2d(n_channel,n_encode,5,2,2),
+            nn.ReLU(),
 
-# Conv Layer
-class ConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super(ConvLayer, self).__init__()
-        padding = kernel_size // 2
-        self.reflection_pad = nn.ReflectionPad2d(padding)
-        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride) #, padding)
+            nn.Conv2d(n_encode,2*n_encode,5,2,2),
+            nn.ReLU(),
 
-    def forward(self, x):
-        out = self.reflection_pad(x)
-        out = self.conv2d(out)
+            nn.Conv2d(2*n_encode,4*n_encode,5,2,2),
+            nn.ReLU(),
+
+            nn.Conv2d(4*n_encode,8*n_encode,5,2,2),
+            nn.ReLU(),
+
+        )
+        self.fc = nn.Linear(8*n_encode*8*8,50)
+
+    def forward(self,x):
+        conv = self.conv(x).view(-1,8*n_encode*8*8)
+        out = self.fc(conv)
         return out
 
-# Upsample Conv Layer
-class UpsampleConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
-        super(UpsampleConvLayer, self).__init__()
-        self.upsample = upsample
-        if upsample:
-            self.upsample = nn.Upsample(scale_factor=upsample, mode='nearest')
-        reflection_padding = kernel_size // 2
-        self.reflection_pad = nn.ReflectionPad2d(reflection_padding)
-        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator,self).__init__()
+        self.fc = nn.Sequential(nn.Linear(n_z+n_l*n_age+n_gender,
+                                          8*8*n_gen*16),
+                                nn.ReLU())
+        self.upconv= nn.Sequential(
+            nn.ConvTranspose2d(16*n_gen,8*n_gen,4,2,1),
+            nn.ReLU(),
 
-    def forward(self, x):
-        if self.upsample:
-            x = self.upsample(x)
-        out = self.reflection_pad(x)
-        out = self.conv2d(out)
+            nn.ConvTranspose2d(8*n_gen,4*n_gen,4,2,1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(4*n_gen,2*n_gen,4,2,1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(2*n_gen,n_gen,4,2,1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(n_gen,n_channel,3,1,1),
+            nn.Tanh(),
+
+        )
+
+    def forward(self,z,age,gender):
+        ## duplicate age & gender conditions as descripted in https://github.com/ZZUTK/Face-Aging-CAAE
+        l = age.repeat(1,n_age).float()
+        k = gender.view(-1,1).repeat(1,n_gender).float()
+
+        x = torch.cat([z,l,k],dim=1)
+        fc = self.fc(x).view(-1,16*n_gen,8,8)
+        out = self.upconv(fc)
         return out
 
-# Residual Block
-#   adapted from pytorch tutorial
-#   https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-
-#   intermediate/deep_residual_network/main.py
-class ResidualBlock(nn.Module):
-    def __init__(self, channels):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
-        self.in1 = nn.InstanceNorm2d(channels, affine=True)
-        self.relu = nn.ReLU()
-        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
-        self.in2 = nn.InstanceNorm2d(channels, affine=True)
 
-    def forward(self, x):
-        residual = x
-        out = self.relu(self.in1(self.conv1(x)))
-        out = self.in2(self.conv2(out))
-        out = out + residual
-        out = self.relu(out)
-        return out 
-
-# Image Transform Network
-class ImageTransformNet(nn.Module):
+class Dimg(nn.Module):
     def __init__(self):
-        super(ImageTransformNet, self).__init__()
-        
-        # nonlineraity
-        self.relu = nn.ReLU()
-        self.tanh = nn.Tanh()
+        super(Dimg,self).__init__()
+        self.conv_img = nn.Sequential(
+            nn.Conv2d(n_channel,n_disc,4,2,1),
+        )
+        self.conv_l = nn.Sequential(
+            nn.ConvTranspose2d(n_l*n_age+n_gender, n_l*n_age+n_gender, 64, 1, 0),
+            nn.ReLU()
+        )
+        self.total_conv = nn.Sequential(
+            nn.Conv2d(n_disc+n_l*n_age+n_gender,n_disc*2,4,2,1),
+            nn.ReLU(),
 
-        # encoding layers
-        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
-        self.in1_e = nn.InstanceNorm2d(32, affine=True)
+            nn.Conv2d(n_disc*2,n_disc*4,4,2,1),
+            nn.ReLU(),
 
-        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
-        self.in2_e = nn.InstanceNorm2d(64, affine=True)
+            nn.Conv2d(n_disc*4,n_disc*8,4,2,1),
+            nn.ReLU()
+        )
 
-        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
-        self.in3_e = nn.InstanceNorm2d(128, affine=True)
+        self.fc_common = nn.Sequential(
+            nn.Linear(8*8*img_size,1024),
+            nn.ReLU()
+        )
+        self.fc_head1 = nn.Sequential(
+            nn.Linear(1024,1),
+            nn.Sigmoid()
+        )
+        self.fc_head2 = nn.Sequential(
+            nn.Linear(1024,n_l),
+            nn.Softmax()
+        )
 
-        # residual layers
-        self.res1 = ResidualBlock(128)
-        self.res2 = ResidualBlock(128)
-        self.res3 = ResidualBlock(128)
-        self.res4 = ResidualBlock(128)
-        self.res5 = ResidualBlock(128)
+    def forward(self,img,age,gender):
+        ## duplicate age & gender conditions as descripted in https://github.com/ZZUTK/Face-Aging-CAAE
+        l = age.repeat(1,n_age,1,1,)
+        k = gender.repeat(1,n_gender,1,1,)
+        conv_img = self.conv_img(img)
+        conv_l   = self.conv_l(torch.cat([l,k],dim=1))
+        catted   = torch.cat((conv_img,conv_l),dim=1)
+        total_conv = self.total_conv(catted).view(-1,8*8*img_size)
+        body = self.fc_common(total_conv)
 
-        # decoding layers
-        self.deconv3 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2 )
-        self.in3_d = nn.InstanceNorm2d(64, affine=True)
+        head1 = self.fc_head1(body)
+        head2 = self.fc_head2(body)
 
-        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2 )
-        self.in2_d = nn.InstanceNorm2d(32, affine=True)
+        return head1,head2
 
-        self.deconv1 = UpsampleConvLayer(32, 3, kernel_size=9, stride=1)
-        self.in1_d = nn.InstanceNorm2d(3, affine=True)
 
-    def forward(self, x):
-        # encode
-        y = self.relu(self.in1_e(self.conv1(x)))
-        y = self.relu(self.in2_e(self.conv2(y)))
-        y = self.relu(self.in3_e(self.conv3(y)))
-
-        # residual layers
-        y = self.res1(y)
-        y = self.res2(y)
-        y = self.res3(y)
-        y = self.res4(y)
-        y = self.res5(y)
-
-        # decode
-        y = self.relu(self.in3_d(self.deconv3(y)))
-        y = self.relu(self.in2_d(self.deconv2(y)))
-        #y = self.tanh(self.in1_d(self.deconv1(y)))
-        y = self.deconv1(y)
-
-        return y
-
-class Vgg16(nn.Module):
+class Dz(nn.Module):
     def __init__(self):
-        super(Vgg16, self).__init__()
-        features = models.vgg16(pretrained=True).features
-        self.to_relu_1_2 = nn.Sequential() 
-        self.to_relu_2_2 = nn.Sequential() 
-        self.to_relu_3_3 = nn.Sequential()
-        self.to_relu_4_3 = nn.Sequential()
+        super(Dz,self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(n_z,n_disc*4),
+            nn.ReLU(),
 
-        for x in range(4):
-            self.to_relu_1_2.add_module(str(x), features[x])
-        for x in range(4, 9):
-            self.to_relu_2_2.add_module(str(x), features[x])
-        for x in range(9, 16):
-            self.to_relu_3_3.add_module(str(x), features[x])
-        for x in range(16, 23):
-            self.to_relu_4_3.add_module(str(x), features[x])
-        
-        # don't need the gradients, just want the features
-        for param in self.parameters():
-            param.requires_grad = False
+            nn.Linear(n_disc*4,n_disc*2),
+            nn.ReLU(),
 
-    def forward(self, x):
-        h = self.to_relu_1_2(x)
-        h_relu_1_2 = h
-        h = self.to_relu_2_2(h)
-        h_relu_2_2 = h
-        h = self.to_relu_3_3(h)
-        h_relu_3_3 = h
-        h = self.to_relu_4_3(h)
-        h_relu_4_3 = h
-        out = (h_relu_1_2, h_relu_2_2, h_relu_3_3, h_relu_4_3)
-        return out
-class discriminator(nn.Module):
-    def __init__(self):
-        super(Vgg16,self).__init__()
-        class Vgg16(nn.Module):
-    def __init__(self):
-        super(Vgg16, self).__init__()
-        features = models.vgg16(pretrained=True).features
-        self.to_relu_1_2 = nn.Sequential() 
-        self.to_relu_2_2 = nn.Sequential() 
-        self.to_relu_3_3 = nn.Sequential()
-        self.to_relu_4_3 = nn.Sequential()
+            nn.Linear(n_disc*2,n_disc),
+            nn.ReLU(),
 
-        for x in range(4):
-            self.to_relu_1_2.add_module(str(x), features[x])
-        for x in range(4, 9):
-            self.to_relu_2_2.add_module(str(x), features[x])
-        for x in range(9, 16):
-            self.to_relu_3_3.add_module(str(x), features[x])
-        for x in range(16, 23):
-            self.to_relu_4_3.add_module(str(x), features[x])
-        
-        # don't need the gradients, just want the features
-        for param in self.parameters():
-            param.requires_grad = False
-
-    def forward(self, x):
-        h = self.to_relu_1_2(x)
-        h_relu_1_2 = h
-        h = self.to_relu_2_2(h)
-        h_relu_2_2 = h
-        h = self.to_relu_3_3(h)
-        h_relu_3_3 = h
-        h = self.to_relu_4_3(h)
-        h_relu_4_3 = h
-        out = (h_relu_1_2, h_relu_2_2, h_relu_3_3, h_relu_4_3)
-        return out
-    
-    def pathway1(self,x):
-        h = self.to_relu_1_2(x)
-        h_relu_1_2 = h
-        path1=(h_relu_1_2)
-        return path1
-    
-    def pathway2(self,x):
-        h = self.to_relu_1_2(x)
-        h_relu_1_2 = h
-        h = self.to_relu_2_2(h)
-        h_relu_2_2 = h
-        path2=(h_relu_2_2)
-        return path2
-    
-    def pathway3(self,x):
-        h = self.to_relu_1_2(x)
-        h_relu_1_2 = h
-        h = self.to_relu_2_2(h)
-        h_relu_2_2 = h
-        h = self.to_relu_3_3(h)
-        h_relu_3_3 = h
-        path3=(h_relu_3_3)
-        return path3
-    
-    def pathway4(self,x):
-        h = self.to_relu_1_2(x)
-        h_relu_1_2 = h
-        h = self.to_relu_2_2(h)
-        h_relu_2_2 = h
-        h = self.to_relu_3_3(h)
-        h_relu_3_3 = h
-        h = self.to_relu_4_3(h)
-        h_relu_4_3 = h
-        path4=(h_relu_4_3)
-        return path4
-
-    def concat():
-        final=path1+path2+path3+path4 
-        return final 
-        
-
-
-
-
-
-
-
-
-
+            nn.Linear(n_disc,1),
+            nn.Sigmoid()
+        )
+    def forward(self,z):
+        return self.model(z)
