4e986748b01870050222b2fe9762f59f3c1da9e9
ajithkumar.v <ajithkumar.v@lotte.net>
2021-01-31 23:09:51 +0530

    colab notebook, fixed errors

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
-# Learning Face Age Progression : A Pyramid Architecture of GANs
-
-Trying to implement code for [Learning Face Age Progression : A Pyramid Architecture of GANs](https://arxiv.org/pdf/1711.10352.pdf) from code of [Face-Aging-CAAE-Pytorch](https://github.com/Jooong/Face-Aging-CAAE-Pytorch)
-
-
+## Face Aging CAAE 
 
 
 ## Requirements
-* pytorch 0.4.0
+
+> pip install -r requirements.txt 
+
+* torchvision 0.4.0
+* torch 1.2.0
 * [UTKFace Aligned&Cropped](https://drive.google.com/drive/folders/0BxYys69jI14kU0I1YUQyY1ZDRUE) dataset
+
+## Colab Notebook
+
+Training - [Colab Notebook](Face_Aging_CAAE_Pytorch_UTK_Face_dataset.ipynb)
+
+Inference - [Colab Notebook]()
+
+**Snapshot**
+
+![Training Snapshot](assests/snapshot_working_FaceCAAE.PNG)
+
+![Testing Snapshot]()
+
 
 
 ## Usage
 * git clone or download zip file of this repository
 * download Aligned & Cropped version of UTKFace from [here](https://drive.google.com/drive/folders/0BxYys69jI14kU0I1YUQyY1ZDRUE)
-* execute main.py in bash
+* Install requirements
+* execute main.py 
 > python main.py
 
 ## Results
@@ -21,10 +35,23 @@ Trying to implement code for [Learning Face Age Progression : A Pyramid Architec
 **UTKFace**
 > rows: years of  0 ~ 5, 5 ~ 10, 10 ~ 15, 16 ~ 20, 21 ~ 30, 31 ~ 40, 41 ~ 50, 51 ~ 60, 61 ~ 70, over 70  
 
-<a href="https://imgur.com/7auIthg"><img src="https://i.imgur.com/7auIthg.png" title="source: imgur.com" /></a>
+```
+epoch:50, step:86
+EG_L1_loss:0.075875           |G_img_loss:5.226651           
+G_tv_loss:0.003358            |Ez_loss:0.851948
+D_img:0.998970                |D_reconst:0.015672            |D_loss:0.017007               
+D_z:0.435863                  |D_z_prior:0.606904            |Dz_loss:1.133016 
+```
+output with 1.7k images trained for 50 epochs
+
+![Epochs](assests/reconst_epoch050.png)
 
 <br></br>
 
 **Irene, Korean Celebrity**
 
 <a href="https://imgur.com/dPpWVf5"><img src="https://i.imgur.com/dPpWVf5.png" title="source: imgur.com" /></a>
+
+
+## Credits
+- [Face-Aging-CAAE-Pytorch](https://github.com/Jooong/Face-Aging-CAAE-Pytorch)
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# this reprository is constructed from CAAE reprository
-# for paper Learning face age progression 
-# 
-
 import torch
 from torch import nn
 from torch import optim
@@ -19,7 +15,7 @@ use_cuda = torch.cuda.is_available()
 makeDir()
 moveFiles()
 
-# loading Images()
+
 dataloader = loadImgs()
 
 ## build model and use cuda if available
@@ -60,8 +56,6 @@ else:
 
 ## fixed variables to regress / progress age
 fixed_l = -torch.ones(80*10).view(80,10)
-
-
 for i,l in enumerate(fixed_l):
     l[i//8] = 1
 
@@ -73,35 +67,26 @@ if use_cuda:
 
 outf='./result_tv_gender'
 
-if os.path.exists(outf):
+if not os.path.exists(outf):
     os.mkdir(outf)
-# iterations =50
+
 niter=50
 
-# iters for 50 epochs
 for epoch in range(niter):
-    # enumerates(dataLoader)
+    print("Epochs", epoch)
     for i,(img_data,img_label) in enumerate(dataloader):
-
+        print("Data loaders - ", i)
         # make image variable and class variable
-        #  img_data -->  
 
-        #  previously age was encoded  and then decoded
         img_data_v = Variable(img_data)
-        #  
-        # age is stored
         img_age = img_label/2
-        # gender is stored
         img_gender = img_label%2*2-1
 
         img_age_v = Variable(img_age).view(-1,1)
         img_gender_v = Variable(img_gender.float())
 
-
         if epoch == 0 and i == 0:
-            # noise data is given 
             fixed_noise = img_data[:8].repeat(10,1,1,1)
-
             fixed_g = img_gender[:8].view(-1,1).repeat(10,1)
 
 
@@ -120,15 +105,11 @@ for epoch in range(niter):
 
         # make one hot encoding version of label
         batchSize = img_data_v.size(0)
-        
-        # (img_age,batch_size=20,n_l=)
         age_ohe = one_hot(img_age,batchSize,n_l,use_cuda)
 
         # prior distribution z_star, real_label, fake_label
         z_star = Variable(torch.FloatTensor(batchSize*n_z).uniform_(-1,1)).view(batchSize,n_z)
-        # real_label 
         real_label = Variable(torch.ones(batchSize).fill_(1)).view(-1,1)
-        # fake_label
         fake_label = Variable(torch.ones(batchSize).fill_(0)).view(-1,1)
 
         if use_cuda:
@@ -141,19 +122,14 @@ for epoch in range(niter):
 
         # EG_loss 1. L1 reconstruction loss
         z = netE(img_data_v)
-        # output is reconstruct
         reconst = netG(z,age_ohe,img_gender_v)
-        #so L1 loss is constructed for output(reconst) and original image data(img_data_v)
         EG_L1_loss = L1(reconst,img_data_v)
 
 
         # EG_loss 2. GAN loss - image
         z = netE(img_data_v)
-        # output of generator is reconst
         reconst = netG(z,age_ohe,img_gender_v)
-        # discriminatoroutput is given as discrimninator_reconst is got from D_img()
         D_reconst,_ = netD_img(reconst,age_ohe.view(batchSize,n_l,1,1),img_gender_v.view(batchSize,1,1,1))
-        # gan loss is given from bce 
         G_img_loss = BCE(D_reconst,real_label)
 
 
@@ -213,12 +189,12 @@ for epoch in range(niter):
 
 
     msg1 = "epoch:{}, step:{}".format(epoch+1,i+1)
-    msg2 = format("EG_L1_loss:%f"%(EG_L1_loss.data[0]),"<30")+"|"+format("G_img_loss:%f"%(G_img_loss.data[0]),"<30")
-    msg5 = format("G_tv_loss:%f"%(G_tv_loss.data[0]),"<30")+"|"+"Ez_loss:%f"%(Ez_loss.data[0])
-    msg3 = format("D_img:%f"%(D_img.mean().data[0]),"<30")+"|"+format("D_reconst:%f"%(D_reconst.mean().data[0]),"<30")\
-    +"|"+format("D_loss:%f"%(D_loss.data[0]),"<30")
-    msg4 = format("D_z:%f"%(Dz.mean().data[0]),"<30")+"|"+format("D_z_prior:%f"%(Dz_prior.mean().data[0]),"<30")\
-    +"|"+format("Dz_loss:%f"%(Dz_loss.data[0]),"<30")
+    msg2 = format("EG_L1_loss:%f"%(EG_L1_loss.data),"<30")+"|"+format("G_img_loss:%f"%(G_img_loss.data),"<30")
+    msg5 = format("G_tv_loss:%f"%(G_tv_loss.data),"<30")+"|"+"Ez_loss:%f"%(Ez_loss.data)
+    msg3 = format("D_img:%f"%(D_img.mean().data),"<30")+"|"+format("D_reconst:%f"%(D_reconst.mean().data),"<30")\
+    +"|"+format("D_loss:%f"%(D_loss.data),"<30")
+    msg4 = format("D_z:%f"%(Dz.mean().data),"<30")+"|"+format("D_z_prior:%f"%(Dz_prior.mean().data),"<30")\
+    +"|"+format("Dz_loss:%f"%(Dz_loss.data),"<30")
 
     print()
     print(msg1)
